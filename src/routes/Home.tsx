39d94f678c46bc2ec327e738aaa1ddc4718cf286
Arthur Kasparian <contact@arthurkasparian.dev>
2023-11-05 00:47:27 +0200

    Mobile Styling Support
    [Add] Some mobile styling in the menu
    [Mod] README.md
    
    Signed-off-by: Arthur Kasparian <contact@arthurkasparian.dev>

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,6 +6,7 @@
 import {NavLink} from "react-router-dom";
 import Feed from "../components/Feed.tsx";
 import websites from "../websites.ts";
+import {Website} from "../interfaces.ts";
 
 const Home = () => {
 
@@ -21,7 +22,33 @@ const Home = () => {
             </nav>
 
             {/*Websites repeated to fill page a little*/}
-            <Feed websites={[...websites, ...websites, ...websites, ...websites, ...websites, ...websites]}/>
+            <Feed websites={[
+                ...websites,
+                ...websites.map(
+                    (website: Website) => {
+                        return {...website, id: website.id + 5}
+                    }
+                ),
+                ...websites.map(
+                    (website: Website) => {
+                        return {...website, id: website.id + 10}
+                    }
+                ),
+                ...websites.map(
+                    (website: Website) => {
+                        return {...website, id: website.id + 15}
+                    }
+                ),
+                ...websites.map(
+                    (website: Website) => {
+                        return {...website, id: website.id + 20}
+                    }
+                ),
+                ...websites.map(
+                    (website: Website) => {
+                        return {...website, id: website.id + 25}
+                    }
+                )]}/>
         </div>
     )
 }
